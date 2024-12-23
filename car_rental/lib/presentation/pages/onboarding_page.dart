3a5d8ff12f98423ce9ca1e2a70391bd3ad9cb841
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2024-12-23 11:47:14 +0530

    Firebase configured and Onboarding page done

---
+++ car_rental/lib/presentation/pages/onboarding_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff2c2B34),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/onboarding.png'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Premium Cars, \nEnjoy the luxury',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.white),
+                      child: Text(
+                        'Let\'s Go',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
